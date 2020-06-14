c8afc31b228f7ba84a51d1f9c40e429af845b2d1
Attila Babo <attila.babo@gmail.com>
2020-06-14 15:02:22 +0200

    Tweak tapping

--- keyboards/handwired/pebble/config.h
+++ keyboards/handwired/pebble/config.h
@@ -29,22 +29,11 @@
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCING_DELAY 5
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-/* key combination for command */
-#define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
-)
-
 /* prevent stuck modifiers */
 #define PREVENT_STUCK_MODIFIERS
 
 // default but important
-#define TAPPING_TERM 200
+#define TAPPING_TERM 225
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
