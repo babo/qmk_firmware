0ede758f3b6b694d85f5dd2c123d81d52c892b81
Attila Babo <attila.babo@gmail.com>
2021-02-10 01:11:36 +0100

    Add shift, increase tapping term

--- keyboards/crkbd/keymaps/babo/config.h
+++ keyboards/crkbd/keymaps/babo/config.h
@@ -10,7 +10,7 @@
 ) \
 LAYOUT( \
 KC_NO, K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   KC_NO, \
-KC_NO, K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   KC_NO, \
+KC_LSFT, K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   KC_RSFT, \
 KC_NO, K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   KC_NO, \
                      K32,   K33,   K34,   K35,   K36,   K37 \
 )
@@ -18,7 +18,7 @@ KC_NO, K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   KC_
 #ifdef TAPPING_TERM
 #undef TAPPING_TERM
 #endif
-#define TAPPING_TERM 180
+#define TAPPING_TERM 210
 
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
